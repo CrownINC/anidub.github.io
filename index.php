2c15aa8e022e849f8410c93118c9159fa2b0f022
CrownINC <36838329+CrownINC@users.noreply.github.com>
2018-02-26 15:19:27 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,126 @@
+﻿
+<!DOCTYPE html> <!-- FGL Lab generate page script v1337 -->
+<html>
+    <head>
+        <title>Молодцы ребята)))</title>
+        <meta charset="utf-8">
+        <link href='https://fonts.googleapis.com/css?family=Ubuntu' rel='stylesheet' type='text/css'>
+        <link href='https://fonts.googleapis.com/css?family=Share+Tech+Mono' rel='stylesheet' type='text/css'>
+        <link href='https://fonts.googleapis.com/css?family=Anonymous+Pro&subset=cyrillic' rel='stylesheet' type='text/css'>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <script type='text/javascript' src='http://code.jquery.com/jquery-latest.min.js'></script>
+        <style>
+            body {
+            	background: #dedede; /* #020200 */
+                padding-top: 20px;
+                padding-bottom: 20px;
+                font-family: 'Ubuntu', sans-serif;
+            }
+            a{
+                color: #5B7FA6;
+            	text-decoration: none;
+            }
+
+            a:hover{
+                color: #405872;
+                text-decoration: none;
+                cursor:pointer;
+            }
+
+            .menu{
+                background: #5B7FA6;
+                width: 960px;
+                height: 20px;
+                color: #fff;
+                text-align: center;
+                font-size: 13px;
+                font-family: 'Share Tech Mono', 'Anonymous Pro', ;
+            }
+            .menu_links_active{
+                color: #fff;
+            }
+            .menu_links_active1{
+                color: #fff;
+            }
+            .menu_links_active:hover{
+                color: #fff;
+                text-decoration: none;
+                cursor:pointer;
+            }
+            .menu_links_active1:hover{
+                color: #e6e6e6;
+                text-decoration: none;
+                cursor:pointer;
+            }
+            .menu_links{
+                color: #fff;
+            }
+            .menu_links:hover{
+                color: #e6e6e6;
+                text-decoration: none;
+                cursor:pointer;
+            }
+            .voronov{width:310px;height:150px;position:fixed;padding:40px 50px 0 0; float:right;bottom:0;z-index:12;overflow:visible;}
+			.voronov_hidden{display:none}
+			.voronov-link{display:block;position:absolute;bottom:-150px;padding:10px 10px 0 10px;width:210px;-webkit-transition:transform .3s ease-out;-moz-transition:transform .3s ease-out;-ms-transition:transform .3s ease-out;-o-transition:transform .3s ease-out;transition:transform .3s ease-out}
+			.voronov-link:hover{-webkit-transform:translateY(-140px);-moz-transform:translateY(-140px);-ms-transform:translateY(-140px);-o-transform:translateY(-140px);transform:translateY(-140px)}
+			.voronov-link:hover .bar__monkey-close{opacity:1}
+			.voronov-img{vertical-align:middle;border:0}
+			.voronov-overlay{padding:40px 50px 0 0; float:right; bottom:1;width:610px;margin-left:10px;z-index:2;}
+			#scrollup {
+			position: fixed;
+			opacity: 0.8;
+			padding: 15px 10px 10px;
+			border-radius: 5px;
+            -webkit-border-radius: 5px;
+            -moz-border-radius: 5px;
+			left: 10px;
+			bottom: 10px;
+			display: none;
+			cursor: pointer;
+			}
+        </style>
+    </head>
+    <body>
+            <center>
+            <div id="specialbg">
+            <div class="menu">
+            <a href="/" class="menu_links_active">#anidub_news</a> | <a href="http://vk.com/scoutr" class="menu_links" target="_blank">#body_flesh</a> | <a href="http://vk.com/paintalk" class="menu_links" target="_blank">Гиперсалатикоглот</a> | <img src="youtube.png" /><a href="https://www.youtube.com/user/makemebad35" class="menu_links">makemebad35</a>
+            </div>
+            <object type=application/x-shockwave-flash data=new.swf width=960 height=488><param name=movie value=new.swf></object></div>
+            <script type="text/javascript" src="//vk.com/js/api/openapi.js?121"></script>
+			<script type="text/javascript">
+  				VK.init({apiId: 5162986, onlyWidgets: true});
+			</script>
+			<div id="vk_comments"></div>
+			<script type="text/javascript">
+				VK.Widgets.Comments("vk_comments", {limit: 10, width: "960", attach: "photo"});
+			</script>
+            </center>
+            <div class="voronov" data-b="32" style="left: 1000px;"><div class="voronov-overlay"></div><a class="voronov-link" href="https://www.youtube.com/watch?v=PEROf44E5fk"><div class="voronov-close"><span class="icon icon_size_M icon_cross"></span></div><img class="voronov-img" src="1.png" srcset="1.png"></a></div>
+            <div id="scrollup"><img alt="Вверх" src="up.png"></div>
+            <script type="text/javascript">
+            window.onload = function() {
+			var scrollUp = document.getElementById('scrollup');
+			scrollUp.onmouseover = function() {
+			scrollUp.style.opacity=0.3;
+			scrollUp.style.filter  = 'alpha(opacity=30)';
+			};
+			scrollUp.onmouseout = function() {
+			scrollUp.style.opacity = 0.5;
+			scrollUp.style.filter  = 'alpha(opacity=50)';
+			};
+			scrollUp.onclick = function() {
+			window.scrollTo(0,0);
+			};
+				window.onscroll = function () {
+				if ( window.pageYOffset > 0 ) {
+				scrollUp.style.display = 'block';
+				} else {
+				scrollUp.style.display = 'none';
+				}
+			};
+			};
+            </script>
+    </body>
+</html>
